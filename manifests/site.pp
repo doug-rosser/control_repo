91bfab9453061ca0b8822ee46b0bdedd74ac39f4
Doug Rosser <doug.rosser@puppetlabs.com>
2014-10-17 14:14:19 -0700

    NEedful stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,3 +47,31 @@ node 'puppet.vm' {
    collect_exported => false,
  }
 }
+
+node /^puppet-master.*/ {
+  class {'puppet_enterprise::profile::master':
+    certname                     => $::clientcert,
+    console_host                 => 'console.vm',
+    puppetdb_host                => 'puppetdb.vm',
+    ca_host                      => 'certificate-authority.vm',
+    classifier_host              => 'console.vm',
+    console_server_certname      => 'console.vm',
+  }
+
+  # The classifier will also add the agent class which will manage the service
+  # which is required for this class as its notifies pe-mcollective
+  include puppet_enterprise::profile::master::mcollective
+
+  # hack until PE-6012 gets fixed
+  #Service <| title == 'pe-puppetserver' |> {
+  #  start => 'service pe-puppetserver start && until test -d /etc/puppetlabs/puppet/ssl/ca ; do sleep 1 ; done'
+  #}
+
+  #file { 'hack for PE-6012':
+  #  ensure  => file,
+  #  path    => "${settings::ssldir}/ca/ca_crt.pem",
+  #  source  => "${settings::ssldir}/certs/ca.pem",
+  #  require => Class['puppet_enterprise::profile::master'],
+  #}
+}
+
