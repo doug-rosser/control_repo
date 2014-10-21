cf91eeaacd9c79335a4859c2b08531a280abcb85
Doug Rosser <doug.rosser@puppetlabs.com>
2014-10-21 13:12:33 -0700

    New cluster info

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,17 +26,17 @@ node 'puppet.vm' {
       },
   }
 
- haproxy::balancermember { "puppet00:puppet-master01.vm":
-   server_names      => "puppet-master01.vm",
+ haproxy::balancermember { "puppet00:new-master01.vm":
+   server_names      => "new-master01.vm",
    listening_service => 'puppet00',
-   ipaddresses       => '172.31.20.62',
+   ipaddresses       => '172.31.23.2',
    ports             => '8140',
    options           => 'check',
  }
- haproxy::balancermember { "puppet00:puppet-master02.vm":
-   server_names      => "puppet-master02.vm",
+ haproxy::balancermember { "puppet00:new-master02.vm":
+   server_names      => "new-master02.vm",
    listening_service => 'puppet00',
-   ipaddresses       => '172.31.20.63',
+   ipaddresses       => '172.31.23.1',
    ports             => '8140',
    options           => 'check',
  }
@@ -46,32 +46,5 @@ node 'puppet.vm' {
    ports            => '8140',
    collect_exported => false,
  }
-}
-
-node /^puppet-master.*/ {
-  class {'puppet_enterprise::profile::master':
-    certname                     => $::clientcert,
-    console_host                 => 'console.vm',
-    puppetdb_host                => 'puppetdb.vm',
-    ca_host                      => 'certificate-authority.vm',
-    classifier_host              => 'console.vm',
-    console_server_certname      => 'console.vm',
-  }
-
-  # The classifier will also add the agent class which will manage the service
-  # which is required for this class as its notifies pe-mcollective
-  include puppet_enterprise::profile::master::mcollective
-
-  # hack until PE-6012 gets fixed
-  #Service <| title == 'pe-puppetserver' |> {
-  #  start => 'service pe-puppetserver start && until test -d /etc/puppetlabs/puppet/ssl/ca ; do sleep 1 ; done'
-  #}
-
-  #file { 'hack for PE-6012':
-  #  ensure  => file,
-  #  path    => "${settings::ssldir}/ca/ca_crt.pem",
-  #  source  => "${settings::ssldir}/certs/ca.pem",
-  #  require => Class['puppet_enterprise::profile::master'],
-  #}
 }
 
